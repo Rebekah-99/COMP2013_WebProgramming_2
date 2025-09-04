4e9fde171530e4befabb47c8cdd0387cbf614eb1
Rebekah-99 <rebekah.driver@yahoo.ca>
2025-09-04 10:30:52 -0400

    This is the first commit

---
+++ Lecture1/helloGit.js
@@ -0,0 +1,8 @@
+//3 Words: Add, Commit, Push
+//Add - add this
+console.log("Hello Git World!");
+//Commit - put a shipping label - "git add ." in terminal window. 
+// This changes "U" (not tracking) to "A" (tracking)
+
+//Push - send it - "git commit -m "whatever string message describes/goes with it"" in the terminal 
+// "git push" will send it to github
